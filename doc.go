b5419592af5c0ec4037bd6154ad9c5440f952c4b
Oliver <oliverrivo@gmail.com>
2014-08-27 11:56:50 +0200

    Starting to test the Image Matcher.

--- doc.go
+++ doc.go
@@ -1,7 +1,9 @@
 /*
 Package duplo provides tools to efficiently query large sets of images for
-visual duplicates (or for visually similiar images).
-
-The technique is based on the paper "Fast Multiresolution Image Querying".
+visual duplicates. The technique is based on the paper "Fast Multiresolution
+Image Querying" by Charles E. Jacobs, Adam Finkelstein, and David H. Salesin,
+with a few modifications and additions. The main goal is to find actual
+duplicates. The search is therefore a bit more strict than if we had to find
+only similar images.
 */
 package duplo
